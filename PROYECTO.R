afa65d5d24a4ba3c2307c524e80b68bce70bff01
CrisTafRos <cristian.tafolla.rosales@gmail.com>
2021-02-04 12:09:05 -0600

    UPDATE: Depuración y corrección

--- PROYECTO.R
+++ PROYECTO.R
@@ -10,24 +10,43 @@ head(DATA)
 str(DATA)
 View(DATA)
 
+# Analizamos la data y la depuramos
+
+a <- gsub("<C1>","",DATA$V1)
+a <- gsub("<C2>","",a)
+a <- gsub("B.1bV---","",a)
+a <- gsub("B.1bP---","",a)
+a <- gsub("D.21-D.31---","",a)
+a <- gsub("Ã¯Â»Â¿","",a)
+
+DATA$V1 <- a
+
 DATOS<-data.frame(t(DATA))
 DATOS
 head(DATOS)
 str(DATOS)
 View(DATOS)
 
-
 suppressMessages(suppressWarnings(library(dplyr)))
 SDATOS<-DATOS[-1,]
 View(SDATOS)
+
+# Analizamos la data y la depuramos
+
+a <- gsub("<R>","",SDATOS$X1)
+a <- gsub("<P>","",a)
+SDATOS$X1 <- a
+
 SDATOS2 <- mutate(SDATOS, X1 = as.Date(X1, "%Y"), X2 = as.numeric(X2), X3 = as.numeric(X3), X4 = as.numeric(X4), X5= as.numeric(X5), X6 = as.numeric(X6), X7 = as.numeric(X7))
 
 View(SDATOS2)
 str(SDATOS2)
-PIB.DATA <- rename(SDATOS2,  AÑO= X1, PIB = X2, ISPN = X3 , VAB=X4, VAB_A1=X5 , VAB_A2=X6, VAB_A3=X7)
+PIB.DATA <- rename(SDATOS2,  AÃ‘O= X1, PIB = X2, ISPN = X3 , VAB=X4, VAB_A1=X5 , VAB_A2=X6, VAB_A3=X7)
 View(PIB.DATA)
 str(PIB.DATA)
+
 data.final<-data.frame(PIB.DATA)
+View(data.final)
 summary(data.final)
 library(DescTools)
 quantile(PIB.DATA$PIB, probs = seq(0.1, 0.9, 0.1))
@@ -44,7 +63,7 @@ hist(x=data.final$PIB,
 
 
 
-#realizando el histograma con la función ggplot()
+#realizando el histograma con la funci?n ggplot()
 ggplot(data.final, aes(PIB))+ 
   geom_histogram(binwidth = 1000000, col="black", fill = "blue") + 
   ggtitle("Histograma de Mediciones") +
@@ -55,8 +74,8 @@ ggplot(data.final, aes(PIB))+
 (my_scatplot <- ggplot(data.final, aes(x = PIB, y = VAB_A1)) + geom_point()+geom_smooth(method = "lm", se = T))
 
 #VEREMOS SI LA VARIABLE PIB ES UNA DISTRIBUCION NORMAL 
-x <- data.final$PIB # Algunos posibles valores que puede tomar la v.a. X (mínimo: mu-4sigma, máximo: mu+4sigma)
-y <- dnorm(x, mean = Mean(data.final$PIB), sd=sd(PIB.DATA$PIB) ) # Valores correspondientes de la función de densidad de probabilidad
+x <- data.final$PIB # Algunos posibles valores que puede tomar la v.a. X (m?nimo: mu-4sigma, m?ximo: mu+4sigma)
+y <- dnorm(x, mean = mean(data.final$PIB), sd=sd(PIB.DATA$PIB) ) # Valores correspondientes de la funci?n de densidad de probabilidad
 
 plot(x, y, type = "l", xlab = "", ylab = "")
 title(main = "Densidad de Probabilidad Normal", sub = expression(paste(mu == 12397738, " y ", sigma == 3448562)))
@@ -81,14 +100,14 @@ p <- ggplot(madf, aes(ma)) +
 p
 
 ma <- rnorm(1000, mean = 12397738, sd = 3448562) 
-# Contraste de hipótesis
+# Contraste de hip?tesis
 
-# Dada una muestra aleatoria de tamaño n = 10 de la distribución normal
+# Dada una muestra aleatoria de tama?o n = 10 de la distribuci?n normal
 
 set.seed(124)
 muestra <- rnorm(n = 10, mean = 12397738, sd = 3448562) 
 tail(as.data.frame(muestra))
-# El valor observado del estadístico de prueba en este caso está dado por
+# El valor observado del estad?stico de prueba en este caso est? dado por
 
 t0 <- (mean(muestra)-12397738)/(sd(muestra)/sqrt(10)) 
 t0
@@ -127,14 +146,14 @@ summary(m3)
 m4<- update(m3, ~.-ISPN)
 summary(m4)
 m4
-# Acontinuación mostramos una matriz de gráficos de dispersión de los
+# A continuaciÃ³n mostramos una matriz de grÃ¡ficos de dispersiÃ³n de los
 # DOS predictores continuos. Los predictores parecen estar linealmente
 # relacionados al menos aproximadamente
 pairs(~ VAB_A1 + VAB_A2, data = data.final, gap = 0.4, cex.labels = 1.5)
 
-# Acontinuación veremos gráficas de residuales estandarizados contra cada
-# predictor. La naturaleza aleatoria de estas gráficas es un indicativo de
-# que el modelo ajustado es un modelo válido para los datos.
+# A continuaciÃ³n veremos grÃ¡ficas de residuales estandarizados contra cada
+# predictor. La naturaleza aleatoria de estas grÃ¡ficas es un indicativo de
+# que el modelo ajustado es un modelo v?lido para los datos.
 
 m4 <- lm(PIB ~ VAB_A1+VAB_A2)
 summary(m4)
@@ -146,14 +165,14 @@ plot(VAB_A1, StanRes1, ylab = "Residuales Estandarizados")
 plot(VAB_A2, StanRes1, ylab = "Residuales Estandarizados")
 dev.off()
 
-# Finalmente mostramos una gráfica de Y, el precio contra los valores
+# Finalmente mostramos una gr?fica de Y, el precio contra los valores
 # ajustados 
 
 plot(m4$fitted.values, PIB, xlab = "Valores ajustados", ylab = "VILES DE PESOS")
 abline(lsfit(m1$fitted.values, PIB))
 
 
-#### Técnicas descriptivas: gráficas, tendencias y variación estacional
+#### T?cnicas descriptivas: gr?ficas, tendencias y variaci?n estacional
 library(TSA)
 data(PIB.DATA)
 seriedetiempo<-ts(data.final$PIB, freq=1, start = c(1980,1) )
@@ -161,11 +180,11 @@ data.final
 seriedetiempo
 
 
-plot(seriedetiempo, type = "o", ylab = "millones de pesos", xlab = "Tiempo", main = "Tendecia de PIB anual")
+plot(seriedetiempo, type = "o", ylab = "millones de pesos", xlab = "Tiempo", main = "Tendencia de PIB anual")
 
 plot(seriedetiempo, type = "l", ylab = "Vmillones de pesos", xlab = "Tiempo",
      main = "Tendecia de PIB anual",
-     sub = "Símbolos Especiales")
+     sub = "SÃ­mbolos Especiales")
 
 
 vab_a1=565151	
@@ -178,7 +197,7 @@ beta4=1.519e+00
 prediccion =beta0+beta3*vab_a1+beta4*vab_a2
 prediccion
 
-plot(x=c(1981:2020), y=data.final$PIB ,type = "o", ylab = "millones de pesos", xlab = "Tiempo", main = "Tendecia de PIB anual")
+plot(x=c(1981:2020), y=data.final$PIB ,type = "o", ylab = "millones de pesos", xlab = "Tiempo", main = "Tendencia de PIB anual")
 points(x=2020,y=prediccion,col="blue",pch=19)
 max(data.final$PIB)
 prediccion
